e7e8488c7c2888c462f0c26ac7854539f5609c7a
Igor Izotikov PC <izotikov.igor1@yandex.ru>
2024-09-03 19:57:09 +0300

    yandex map connected

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ phoneInputs.forEach((item) => {
 ymaps.ready(init);
 function init(){
     // Создание карты.
-    var myMap = new ymaps.Map("map", {
+    var map = new ymaps.Map("map", {
         // Координаты центра карты.
         // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
@@ -40,10 +40,30 @@ function init(){
         zoom: 16
     });
 
-    var myPlacemark = new ymaps.Placemark([59.943543,30.338928], {}, {
+    var myPlacemark = new ymaps.Placemark([59.943543,30.338928],
+        {
+          balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>`
+        },
+        {
         iconLayout: 'default#image',
         iconImageHref: './img/map/location-pin.svg',
-        iconImageSize: [30, 42],
-        iconImageOffset: [-3, -42]
+        iconImageSize: [40, 40],
+        iconImageOffset: [-20, -40]
     });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+    map.geoObjects.add(myPlacemark);
 }
