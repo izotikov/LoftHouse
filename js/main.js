90be0f396da872cd220957939ced588bd81e96d4
Igor Izotikov PC <izotikov.igor1@yandex.ru>
2024-09-03 16:28:35 +0300

    Adaptive header and appartments block

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// header__top-row--mobile
+//
+// nav-icon-active
+
+const navButton = document.querySelector('.nav-icon-btn');
+const navIcon = navButton.querySelector('.nav-icon');
+const nav = document.querySelector('.header__top-row');
+
+navButton.addEventListener('click', ()=> {
+    navIcon.classList.toggle('nav-icon--active');
+    nav.classList.toggle('header__top-row--mobile');
+    document.body.classList.toggle('no-scroll');
+})
+
